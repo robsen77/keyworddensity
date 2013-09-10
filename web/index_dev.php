3aed2271a5f20cba5bfff0ca21dfb3042d7d692e
Robert Bernhard <bloddynewbie@gmail.com>
2013-09-10 20:19:39 +0200

    trashed old skeleton and added new one

--- web/index_dev.php
+++ web/index_dev.php
@@ -2,11 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+Symfony\Component\Debug\Debug::enable();
+
 $app = new Silex\Application();
 
-require __DIR__.'/../resources/config/prod.php';
+require __DIR__.'/../resources/config/dev.php';
 require __DIR__.'/../src/app.php';
 
 require __DIR__.'/../src/controllers.php';
 
-$app['http_cache']->run();
+$app->run();
